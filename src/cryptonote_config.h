a15588aa0f701ca4b81bc78cead5e2d389cdc4d1
FestivalCoinDev <41481370+groovefest@users.noreply.github.com>
2018-08-24 18:38:21 +0430

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -11,7 +11,7 @@
 #define BLOCK_MAJOR_VERSION_4                           4
 
 #define COIN                                            ((uint64_t)100000000) // pow(10, 8)
-#define DEFAULT_FEE                                     ((uint64_t)1000000) // pow(10, 6)
+#define DEFAULT_FEE                                     ((uint64_t)100000) // pow(10, 6)
 
 
 #define CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW            60
